99cae502a7ce432fc150cfa2dd1a55322032b95e
Juanprb13 <juanprb1302@gmail.com>
2021-09-22 17:42:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-l56xmx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-l56xmx)
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
+import { CreateEmpleadoComponent } from './components/create-empleado/create-empleado.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    ListEmpleadosComponent,
+    CreateEmpleadoComponent,
+    NavbarComponent,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
